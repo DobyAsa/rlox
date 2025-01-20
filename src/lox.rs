1b6ad8a560b46536ccd3e851da93c27ce35dabb4
Doby <hjjhubu@qq.com>
2025-01-20 23:30:52 +0800

    changes

--- src/lox.rs
+++ src/lox.rs
@@ -1,5 +1,169 @@
-use std::{fs, io};
 use crate::Args;
+use std::{fs, io};
+
+struct Scanner {
+    source: String,
+    tokens: Vec<Token>,
+
+    start: usize,
+    current: usize,
+    line: usize,
+}
+
+impl Scanner {
+    pub fn new(source: String) -> Scanner {
+        Scanner {
+            source,
+            tokens: vec![],
+            start: 0,
+            current: 0,
+            line: 1,
+        }
+    }
+
+    pub fn scan_tokens(&mut self) -> Vec<Token> {
+        let mut tokens: Vec<Token> = vec![];
+
+        while !self.at_end() {
+            self.start = self.current;
+            self.scan_token();
+        }
+
+        tokens.push(Token::new(TokenType::Eof, "", self.line));
+        tokens
+    }
+
+    fn scan_token(&mut self) {
+        let c: char = self.advance();
+
+        match c {
+            '(' => self.add_token(TokenType::LeftParen),
+            ')' => self.add_token(TokenType::RightParen),
+            '{' => self.add_token(TokenType::LeftBrace),
+            '}' => self.add_token(TokenType::RightBrace),
+            ',' => self.add_token(TokenType::Comma),
+            '.' => self.add_token(TokenType::Dot),
+            '-' => self.add_token(TokenType::Minus),
+            '+' => self.add_token(TokenType::Plus),
+            ';' => self.add_token(TokenType::Semicolon),
+            '*' => self.add_token(TokenType::Star),
+            '!' => {
+                if self.next_char('=') {
+                    self.add_token(TokenType::BangEqual)
+                } else {
+                    self.add_token(TokenType::Bang)
+                }
+            },
+            '=' => {
+                if self.next_char('=') {
+                    self.add_token(TokenType::EqualEqual)
+                } else {
+                    self.add_token(TokenType::Equal)
+                }
+            }
+            default => {
+                Lox::error(self.line, "Unrecognized character.");
+            }
+        }
+    }
+
+    fn next_char(&mut self, expected: char) -> bool {
+        if self.at_end() {
+            return false;
+        }
+
+        if self.source.chars().nth(self.current) != Some(expected) {
+            return false;
+        }
+
+        self.current += 1;
+        true
+    }
+
+    fn advance(&mut self) -> char {
+        let c: char = self.source.chars().nth(self.current).unwrap();
+        self.current += 1;
+        c
+    }
+
+    fn add_token(&mut self, token_type: TokenType) {
+        self.tokens.push(Token::new(
+            token_type,
+            &self.source[self.start..self.current],
+            self.line,
+        ));
+    }
+
+    fn at_end(&self) -> bool {
+        self.current >= self.source.len()
+    }
+}
+
+enum TokenType {
+    // single character tokens
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // one or two character tokens
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // literals
+    Identifier,
+    String,
+    Number,
+
+    // Keywords
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    Eof,
+}
+
+struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    line: usize,
+}
+
+impl Token {
+    fn new(token_type: TokenType, lexeme: &str, line: usize) -> Token {
+        Token {
+            token_type,
+            lexeme: String::from(lexeme),
+            line,
+        }
+    }
+}
 
 pub struct Lox {
     had_error: bool,
@@ -7,14 +171,10 @@ pub struct Lox {
 
 impl Lox {
     pub fn new() -> Lox {
-        Lox {
-            had_error: false
-        }
+        Lox { had_error: false }
     }
 
-    pub fn exec(&self, args: &Args) {
-
-    }
+    pub fn exec(&self, args: &Args) {}
     fn run_file(&mut self, path: &str) -> io::Result<()> {
         let bytes = fs::read(path)?;
 
@@ -22,6 +182,11 @@ impl Lox {
             let line = line.trim();
             self.run(line);
         }
+
+        if self.had_error {
+            panic!("File run failed");
+        }
+
         Ok(())
     }
 
@@ -42,11 +207,11 @@ impl Lox {
         println!("Running: {}", input);
     }
 
-    fn error(&self, line: i32, message: &str) {
-        self.report(line, "", message);
+    fn error(line: usize, message: &str) {
+        Lox::report(line, "", message);
     }
 
-    fn report(&self, line: i32, position: &str, message: &str) {
+    fn report(line: usize, position: &str, message: &str) {
         eprintln!("[line {}] Error: {}: {}", line, position, message);
     }
 }
